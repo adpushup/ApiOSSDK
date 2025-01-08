e33f96c781db8051b635248edfb5e2f400290939
rishh18 <rishabh.tripathi@adpushup.com>
2025-01-08 22:16:37 +0530

    Required Changes

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "ApMobileSDK",
             url: "https://github.com/rishh18/ApMobileSDK/releases/download/0.0.1/ApMobileSDK.xcframework.zip",
-            checksum: "dd04ef1f6cfd32f8885b72474330f417"
+            checksum: "e5e6c36b9b7977638bc1823680ab672c"
         )
     ]
 )
