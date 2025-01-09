8095db2e85372d0fe2f8249dd2205e9f5ae7a3fc
Ankush Kumar <89838353+ankushkumar5@users.noreply.github.com>
2025-01-09 15:59:20 +0530

    1.0.0 Release (#4)
    * Create LICENSE
    
    * initial commit (#1)
    
    * initial commit
    
    * Required Changes
    
    * Final Changes
    
    * Dependency Added
    
    * Binary URL updated
    
    * Readme Added (#3)
    
    * Readme Added
    
    * package file updated
    
    * podspec change
    
    * Readme changes
    
    ---------
    
    Co-authored-by: rishh18 <rishabh.tripathi@adpushup.com>

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ApMobileSDK",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "ApMobileSDK",
+            targets: ["ApMobileSDK"]
+        ),
+    ],
+    dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ApMobileSDK",
+            url: "https://github.com/adpushup/ApiOSSDK/releases/tag/1.0.0/ApMobileSDK.xcframework.zip",
+            dependencies: [.product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")],
+            checksum: "e5e6c36b9b7977638bc1823680ab672c"
+        )
+    ]
+)
