ccc5c28e28509f57ece3f0481f8a7416ca7d4e58
rishh18 <rishabh.tripathi@adpushup.com>
2025-01-24 11:07:39 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,11 +11,10 @@ let package = Package(
     products: [
         .library(
             name: "ApMobileSDK",
-            targets: ["ApMobileSDK"]
+            targets: ["Sources/ApiOSSDK"]
         ),
     ],
-    dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0"),
-    ],
+    dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")],
     targets: [
         .binaryTarget(
             name: "ApMobileSDK",
@@ -23,11 +22,11 @@ let package = Package(
             checksum: "7e859ac1fac5cb4444fee0891e8822af1a2bd519e0e710bfa977aa735881fb07"
         ),
         .target(
-          name: "Sources",
+          name: "Sources/ApiOSSDK",
           dependencies: [
-                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")
           ],
-          path: "Sources"
+          path: "Sources/ApiOSSDK"
         )
     ]
 )
