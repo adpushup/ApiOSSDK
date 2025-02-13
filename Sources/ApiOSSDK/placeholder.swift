8b1f5114ad0fb8cd434961a73a9a4dbf8ad63fa8
rishh18 <rishabh.tripathi@adpushup.com>
2025-02-13 16:06:08 +0530

    1.0.3 Release

--- Sources/ApiOSSDK/placeholder.swift
+++ Sources/ApiOSSDK/placeholder.swift
@@ -8,16 +8,20 @@
 // xcodebuild archive \
 // -workspace ApMobileSDK.xcworkspace \
 // -scheme ApMobileSDK \
+// -configuration Release \
 // -archivePath "./build/iOS" \
 // -sdk iphoneos \
-// SKIP_INSTALL=NO
+// SKIP_INSTALL=NO \
+// BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 // xcodebuild archive \
 // -workspace ApMobileSDK.xcworkspace \
 // -scheme ApMobileSDK \
+// -configuration Release \
 // -archivePath "./build/iOSSimulator" \
 // -sdk iphonesimulator \
-// SKIP_INSTALL=NO
+// SKIP_INSTALL=NO \
+// BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 // xcodebuild -create-xcframework \
 //     -framework "./build/iOS.xcarchive/Products/Library/Frameworks/ApMobileSDK.framework" \
