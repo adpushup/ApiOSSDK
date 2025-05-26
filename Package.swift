ef7ad090ac58d1d16b4e0e85da11f848ab6cb6dc
rishh18 <rishabh.tripathi@adpushup.com>
2025-05-26 16:22:35 +0530

    Version 1.0.23

--- Package.swift
+++ Package.swift
@@ -10,25 +10,26 @@ let package = Package(
     products: [
         .library(
             name: "ApiOSSDK",
-            targets: ["ApiOSSDK"]
+            targets: ["ApMobileSDK"]
         ),
     ],
     dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")],
     targets: [
         .binaryTarget(
             name: "ApMobileSDK",
-            url: "https://github.com/adpushup/ApiOSSDK/releases/download/1.0.22/ApMobileSDK.xcframework.zip",
+            url: "https://github.com/adpushup/ApiOSSDK/releases/download/1.0.23/ApMobileSDK.xcframework.zip",
             checksum: "b6ef09b83dc1df35a955669f3818a245610d907eb07ebef756b9a3d53dc6c07f"
-        ),
-        .target(
-          name: "ApiOSSDK",
-          dependencies: [.target(name: "ApMobileSDK"),
-                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
-          ],
-          path: "Sources/ApiOSSDK",
-          sources: ["placeholder.swift"],
-          resources: [],
-          publicHeadersPath: nil
         )
+        // ,
+        // .target(
+        //   name: "ApiOSSDK",
+        //   dependencies: [.target(name: "ApMobileSDK"),
+        //         .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+        //   ],
+        //   path: "Sources/ApiOSSDK",
+        //   sources: ["placeholder.swift"],
+        //   resources: [],
+        //   publicHeadersPath: nil
+        // )
     ]
 )
--- Sources/ApiOSSDK/placeholder.swift
+++ Sources/ApiOSSDK/placeholder.swift
@@ -5,6 +5,4 @@
 //  Created by Rishabh Tripathi on 23/01/25.
 //
 
-import Foundation
-@_exported import ApMobileSDK
-@_exported import GoogleMobileAds
+
